d8cc30051e47aab69e54a03525b41d3e68d3170a
Chenerywoman <rachelmchenery@hotmail.com>
2023-11-08 18:13:10 +0000

    complete Daniel bingo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let danielWords = ['lego', 'bike', 'football', 'chocolate', 'portal', 'den', 'cheeky', 'mega-hega', 'huggle', 'David Walliams', 'reading', 'creative', 'fun', 'mummy\'s jumper', 'Oswald Road', 'beaver camp', 'swimming', 'hungry', 'running fast', 'questions', 'thoughtful', 'happy', 'ice-cream', 'custard', 'Roald Dahl'];
+let danielWords = ['lego', 'bike', 'football', 'chocolate', 'portal', 'den', 'cheeky', 'mega-hega', 'huggle', 'David Walliams', 'reading', 'creative', 'fun', 'mummy\'s jumper', 'Oswald Road', 'beaver camp', 'swimming', 'hungry', 'running fast', 'questions', 'thoughtful', 'happy', 'ice-cream', 'custard', 'Roald Dahl', 'Alexandra Park', 'Hartley Hall Gardens', 'Grandad', 'Uncle Neil', 'Aunty Becky', 'Operation Ouch', 'Gross Alert!', 'class camping', 'cubs', 'Ruthie', 'Eiffel Tower', 'trampoline', 'apples', 'trains', 'drawing', 'Owl Club', 'Early Birds', 'Mrs Eldridge', 'sausages', 'pancakes', 'Norway', 'sledging', 'Ross Welford', 'Horrible Histories'];
 
 makeRandom = (array) => {
     for (i = array.length - 1; i > 0; i--) {
@@ -14,9 +14,8 @@ let randomized = makeRandom(danielWords).split(",");
 
 let boxes = "";
 for (let i = 0; i < randomized.length; i++) {
-    boxes += '<div class="words">' + randomized[i] + '</div ></br>';
+    boxes += '<div class="words">' + randomized[i] + '</div >';
 }
 
-console.log(randomized)
-console.log(boxes)
 document.getElementById("bingo-card").innerHTML = boxes
+console.log(danielWords.length)
